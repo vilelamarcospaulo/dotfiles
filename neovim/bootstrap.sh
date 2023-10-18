af053ca49748e58dc82aa5b91615d8e7af9f63a4
vilelamarcospaulo <marcospaulo.vilelasantos@gmail.com>
2023-10-18 11:21:10 -0300

    chore: remove packer since neovim config uses lazy now

--- neovim/bootstrap.sh
+++ neovim/bootstrap.sh
@@ -2,14 +2,6 @@ source $PWD/aux.sh
 
 check_and_install_command "nvim" "neovim"
 
-packer_dir=~/.local/share/nvim/site/pack/packer/start/packer.nvim
-if [ ! -d $packer_dir ]; then
-  git clone --depth 1 https://github.com/wbthomason/packer.nvim $packer_dir
-  echo "Packer installed"
-else
-  echo "Packer already installed"
-fi
-
 neovim_dir=~/.neovim
 if [ ! -d $neovim_dir ]; then
   git clone git@github.com:vilelamarcospaulo/.neovim.git ~/.neovim
@@ -18,7 +10,4 @@ else
 fi
 
 ln -s ~/.neovim ~/.config/nvim
-
-messages=("Remember to run :PackeSync inside nvim" "Remember to run :checkhealth inside nvim")
-warning "${messages[@]}"
 
