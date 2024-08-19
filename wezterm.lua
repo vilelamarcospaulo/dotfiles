e0cddbc30a6d5be75c5988c97f89797d574479fb
vilelamarcospaulo <marcospaulo.vilelasantos@gmail.com>
2024-08-19 11:15:39 -0300

    disable ligatures

--- wezterm.lua
+++ wezterm.lua
@@ -5,13 +5,15 @@ local config = wezterm.config_builder()
 
 config.color_scheme = 'Default Dark (base16)'
 
-config.font = wezterm.font 'FiraMono Nerd Font Mono'
-config.font_size = 14.0
+config.font = wezterm.font 'Iosevka'
+config.font_size = 15.0
+config.harfbuzz_features = { "calt=0", "clig=0", "liga=0" }
 
 -- background_opacity
-config.window_background_opacity = 0.95
+config.window_background_opacity = 0.90
 config.macos_window_background_blur = 20
 
+config.audible_bell = "Disabled"
 config.enable_tab_bar = true
 config.default_cursor_style = 'SteadyUnderline'
 -- config.hide_tab_bar_if_only_one_tab = true
@@ -58,7 +60,7 @@ end
 
 wezterm.on(
   'format-tab-title',
-  function(tab, tabs, panes, config, hover, max_width)
+  function(tab, tabs, panes, _config, hover, max_width)
     local background = '#333333'
     local foreground = '#808080'
     local title = tab.active_pane.title
