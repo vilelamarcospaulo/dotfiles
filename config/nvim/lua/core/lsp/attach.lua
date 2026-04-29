d768f8cf07a4efde790d7d5472f87371c1fd67a7
vilelamarcospaulo <marcospaulo.vilelasantos@gmail.com>
2026-04-29 16:05:20 -0300

    use vim.lsp 0.11 api

---
+++ config/nvim/lua/core/lsp/attach.lua
@@ -0,0 +1,47 @@
+local fzf = require("fzf-lua")
+
+vim.api.nvim_create_autocmd('LspAttach', {
+  callback = function(ev)
+    local buffer = ev.buf
+    local client = vim.lsp.get_client_by_id(ev.data.client_id)
+    if not client then
+      return
+    end
+
+    -- completion
+    if client:supports_method(vim.lsp.protocol.Methods.textDocument_completion) then
+      vim.opt.completeopt = { 'menu', 'menuone', 'noinsert', 'fuzzy', 'popup', 'preview' }
+      vim.lsp.completion.enable(true, client.id, buffer, { autotrigger = true })
+    end
+
+    -- format
+    if client:supports_method(vim.lsp.protocol.Methods.textDocument_formatting) then
+      vim.api.nvim_create_autocmd('BufWritePre', {
+        buffer = buffer,
+        callback = require('core.lsp.format_marks')
+      })
+    end
+
+    -- navigation
+    local opts = { buffer = buffer }
+    vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
+    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+    vim.keymap.set('n', 'gr', fzf.lsp_references, opts)
+    vim.keymap.set('n', '<C-p>', fzf.lsp_live_workspace_symbols)
+    vim.keymap.set('n', 'gi', fzf.lsp_implementations, opts)
+
+    -- modifiers
+    vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, opts)
+    vim.keymap.set({ 'n', 'v' }, '<leader>a', fzf.lsp_code_actions, opts)
+
+    -- diagnostics
+    vim.keymap.set('n', '<leader>d', vim.diagnostic.open_float)
+    vim.keymap.set('n', '[d', function() vim.diagnostic.jump({ count = -1, float = true }) end)
+    vim.keymap.set('n', ']d', function() vim.diagnostic.jump({ count = 1, float = true }) end)
+    vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist)
+
+    -- info
+    vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+    vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
+  end,
+})
--- config/nvim/lua/plugins/lspconfig.lua
+++ config/nvim/lua/plugins/lspconfig.lua
@@ -1,68 +1,6 @@
 return {
   'neovim/nvim-lspconfig',
-  dependencies = {
-    "saghen/blink.cmp",
-  },
-  config = function()
-    local lspconfig = require('lspconfig')
-    local capabilities = require("blink.cmp").get_lsp_capabilities()
+  config = function() 
 
-    lspconfig.clojure_lsp.setup {
-      capabilities = capabilities,
-      root_dir = function(pattern)
-        local util = require("lspconfig.util")
-        local fallback = vim.loop.cwd()
-        local patterns = { "project.clj", "deps.edn", "build.boot", "shadow-cljs.edn", ".git", "bb.edn" }
-
-        local root = util.root_pattern(patterns)(pattern)
-        return (root or fallback)
-      end
-    }
-
-    lspconfig.gopls.setup {
-      capabilities = capabilities,
-      cmd = { 'gopls', 'serve' },
-      settings = {
-        gopls = {
-          analyses = {
-            unusedparams = true,
-          },
-          staticcheck = true,
-        },
-      },
-    }
-
-    lspconfig.lua_ls.setup {
-      capabilities = capabilities,
-      settings = {
-        Lua = {
-          diagnostics = {
-            globals = { 'vim' },
-          },
-          workspace = {
-            library = {
-              [vim.fn.expand '$VIMRUNTIME/lua'] = true,
-              [vim.fn.stdpath 'config' .. '/lua'] = true,
-            },
-          },
-        },
-      }
-    }
-
-    lspconfig.pylsp.setup {
-      capabilities = capabilities,
-      settings = {
-        pylsp = {
-          plugins = {
-            black = { enabled = true },
-            isort = { enabled = true },
-          }
-        }
-      }
-    }
-
-    lspconfig.ts_ls.setup {
-      capabilities = capabilities,
-    }
   end
 }
